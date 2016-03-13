93512f83c17cde2e2785b0141e70940b10c26f4d
dandingol03 <1291248710@qq.com>
2016-03-13 23:54:59 +0800

    template commit

---
+++ app$yzts/user/js/app.js
@@ -0,0 +1,62 @@
+/**
+ * Created by outstudio on 16/3/9.
+ */
+
+
+
+//customer sidebar menu plugin
+$.fn.extend({
+    tree:function(){
+        return this.each(function() {
+            var btn = $(this).children("a").first();
+            var menu = $(this).children(".treeview-menu").first();
+            var isActive = $(this).hasClass('active');
+
+            //initialize already active menus
+            if (isActive) {
+                menu.show();
+                btn.children(".fa-angle-left").first().removeClass("fa-angle-left").addClass("fa-angle-down");
+            }
+            //Slide open or close the menu on link click
+            btn.click(function(e) {
+                e.preventDefault();
+                if (isActive) {
+                    //Slide up to close menu
+                    menu.slideUp();
+                    isActive = false;
+                    btn.children(".fa-angle-down").first().removeClass("fa-angle-down").addClass("fa-angle-left");
+                    btn.parent("li").removeClass("active");
+                } else {
+                    //Slide down to open menu
+                    menu.slideDown();
+                    isActive = true;
+                    btn.children(".fa-angle-left").first().removeClass("fa-angle-left").addClass("fa-angle-down");
+                    btn.parent("li").addClass("active");
+                }
+            });
+
+            /* Add margins to submenu elements to give it a tree look */
+            menu.find("li > a").each(function() {
+                var pad = parseInt($(this).css("margin-left")) + 10;
+
+                $(this).css({"margin-left": pad + "px"});
+            });
+
+        });
+    }
+
+});
+
+//使用sidebar-menu的tree函数
+$(".sidebar .treeview").tree();
+
+
+//use slimscroll within jquery.slimscroll.js plugin
+$(".navbar .menu").slimscroll({
+    height: "200px",
+    alwaysVisible: false,
+    size: "3px"
+}).css("width", "100%");
+
+//Activate tooltips
+$("[data-toggle='tooltip']").tooltip();
